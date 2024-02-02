cadd04bfffb56e101b7f363a6ce8beb412b2443c
rodersnow <rodersnow@gmail.com>
2024-02-02 10:34:26 +0100

    fetch get info from the webserver

--- frontend/main.py
+++ frontend/main.py
@@ -2,18 +2,23 @@
 import requests
 import json
 
-def get_data():
-    url = "http://127.0.0.1:5000"
-    headers = {'Content-Type': 'application/json'}
+url = "http://127.0.0.1:5000/"
+headers = {'Content-Type': 'application/json'}
+
+# Simulation time (one day = a certain number of seconds)
+# Total energy consumption
+# EV battery charging in kWh
+def get_info():
     
     # Send GET request to the server
-    get_response = requests.get(url)
+    get_response = requests.get(url + "info")
     
     # Check if the GET request was successful (status code 200)
     if get_response.status_code == 200:
         # Parse JSON response for GET
         data = get_response.json()
-        print(data["name"])
+        print(data)
     
     else:
         print(f"Error after GET: {get_response.status_code}")
+
