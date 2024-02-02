379d326f9bdc660f4b51f5bc8d21b0b9557a7ed4
rodersnow <rodersnow@gmail.com>
2024-02-02 11:50:12 +0100

    method start_charge
    this post request will start the session

--- frontend/main.py
+++ frontend/main.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 url = "http://127.0.0.1:5000/"
-headers = {'Content-Type': 'application/json'}
+
 
 # Simulation time (one day = a certain number of seconds)
 # Total energy consumption
@@ -66,3 +66,20 @@ def get_charge():
     
     else:
         print(f"Error after GET: {get_response.status_code}")
+
+
+def start_charge():
+    headers = {'Content-Type': 'application/json'}
+    
+    # JSON request payload
+    payload = {'charging': 'on'}
+    
+    # Send POST request to the server
+    response = requests.post(url + "charge", data=json.dumps(payload), headers=headers)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse JSON response and deliver dict or list to result
+        data = response.json()
+        print(data)
+
